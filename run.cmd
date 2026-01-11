2e4435cd789beea8e48f2ba992b86e41c943f88d
Marthyboo <128302830+Marthyboo@users.noreply.github.com>
2026-01-11 14:27:16 +0000

    Delete run.cmd

--- run.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-if not exist target\veneclicker-1.0-SNAPSHOT.jar (
-    echo Jar file not found! Please run build.cmd first.
-    pause
-    exit /b 1
-)
-echo Running veneclicker...
-java -jar target\veneclicker-1.0-SNAPSHOT.jar
-pause
